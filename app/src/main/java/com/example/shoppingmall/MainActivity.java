372fa75871ef41da2e0f03c45a54cce01cccc214
SEUNGOH <SeungOhLEE>
2021-10-05 15:03:09 +0900

    2021-10-05

--- app/src/main/java/com/example/shoppingmall/MainActivity.java
+++ app/src/main/java/com/example/shoppingmall/MainActivity.java
@@ -7,7 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
+
 import com.google.firebase.auth.FirebaseAuth;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,9 +33,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        });
 
+        });
         //탈퇴 처리
         //mFirebaseAuth.getCurrentUser().delete();
     }
 }
+
